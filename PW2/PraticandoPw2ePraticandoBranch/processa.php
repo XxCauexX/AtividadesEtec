120cc3f9d94739677e9d899b13346a79cad7d739
Cauê de Andrade <caue.andrade.505@gmail.com>
2023-05-19 09:48:39 -0300

    fazendo a logica do processo

---
+++ PW2/PraticandoPw2ePraticandoBranch/processa.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+        if (filter_input(SERVER_INPUT,'REQUEST_METHOD') = "POST") {
+            echo "deu bom";
+        } else {
+            echo "deu ruim";
+        }
+    ?>
+</body>
+</html>
